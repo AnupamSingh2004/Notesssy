256dbcfc3a3eeacf8e27fbb0cdcf9d1dea89b7dc
anupam <sanupam2004@gmail.com>
2025-03-27 18:06:33 +0530

    Auth Page Files

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,18 +1,20 @@
+
 import AuthForm from '@/components/AuthForm';
 import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import React from 'react'
 
-function LoginPage() {
+function SignUpPage() {
   return (
     <div className='mt-20 flex flex-col items-center'>
       <Card className='w-full max-w-md'>
         <CardHeader className='mb-4'>
-          <CardTitle className='text-center text-3xl'>Login</CardTitle>
+          <CardTitle className='text-center text-3xl'>Sign Up</CardTitle>
         </CardHeader>
-        <AuthForm type="login" />
+        <AuthForm type="register" />
       </Card>
     </div>
   )
 };
 
-export default LoginPage;
+export default SignUpPage;
+
